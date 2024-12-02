b768d67cbc03300bc0ddab527e257c226f18b019
Alien314 <stealth131l30@yahoo.com>
2024-12-01 21:27:17 -0600

    fixed Caps

---
+++ addons/compat_ace_fire/XEH_PREP.hpp
@@ -0,0 +1,1 @@
+[QPATHTOF(functions\fnc_burnSimulation.sqf), QUOTE(DOUBLES(REPLACEMOD,fnc_burnSimulation))] call CBA_fnc_compileFunction;
---
+++ addons/compat_ace_overpressure/XEH_PREP.hpp
@@ -0,0 +1,2 @@
+[QPATHTOF(functions\fnc_overpressureDamage.sqf), QUOTE(DOUBLES(REPLACEMOD,fnc_overpressureDamage))] call CBA_fnc_compileFunction;
+[QPATHTOF(functions\fnc_firedEHBB.sqf), QUOTE(DOUBLES(REPLACEMOD,fnc_firedEHBB))] call CBA_fnc_compileFunction;
