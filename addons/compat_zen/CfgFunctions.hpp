472177fba2c99e2cda9b089fb1d746a0151c37b1
diwako <diwako@gmail.com>
2025-06-24 11:44:40 +0200

    Fix ZEN context menu heal (#149)
    * Fix ZEN context menu heal
    
    * reuse code for healing module
    
    * appease the linter

---
+++ addons/compat_zen/CfgFunctions.hpp
@@ -0,0 +1,10 @@
+class CfgFunctions {
+    class overwrite_zen_context_actions {
+        tag = "zen_context_actions";
+        class zen_context_actions {
+            class healUnits {
+                file = QPATHTOF(functions\fnc_healUnits.sqf);
+            };
+        };
+    };
+};
